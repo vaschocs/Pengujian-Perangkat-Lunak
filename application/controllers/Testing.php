3057704ea95e21352185c9312bd59e39dbcd0ec6
Argo Wibowo <argo.wibowo1989@gmail.com>
2020-02-18 18:49:49 +0700

    Tugas Minggu 3 = Conditional Testing and Looping Testing

--- application/controllers/Testing.php
+++ application/controllers/Testing.php
@@ -34,6 +34,14 @@ class Testing extends CI_Controller
 
         //$this->unit->run($output,$expect,$test_name);
         $this->unit->run($true,$expected,$test_name);
+
+        $test_name = 'tes if else';
+        $this->unit->run($this->ifelse('tes','halo'),'tes',$test_name);
+
+        $test_name = 'tes loop 2';
+        $arr = array(0,1,2,3,4);
+        $this->unit->run($this->loop2($arr),4,$test_name);
+
         echo $this->unit->report();
     }
 
@@ -48,5 +56,95 @@ class Testing extends CI_Controller
         $this->output
         ->set_content_type('application/json')
         ->set_output(json_encode(array('foo' => 'bar')));
+    }
+
+    //minggu 3
+    public function ifelse($namachief = null,$namarm = null){
+        $tmp = '';
+        if($namachief != NULL){
+            $tmp = $namachief;
+        }
+        else if ($namarm != NULL){
+           $tmp = $namarm;
+        }
+        else{
+            $tmp = "Something wrong. Please contact US";
+        }
+        return $tmp;
+    }
+
+    public function ifelse2($teman){
+        $tmp = '';
+        if($teman == "andi"){
+            $tmp = "dia adalah teman saya";
+        }else{
+            $tmp = "dia bukan teman saya";
+        }
+        return $tmp;
+    }
+
+    public function ifelse3($namachief = null, $namarm = null, $namamhs){
+        $tmp = '';
+        if($namachief != NULL){
+            $tmp = $namachief;
+        }
+        else if ($namarm != NULL){
+            $tmp = $namarm;
+        }
+        else if ($namamhs != NULL){
+            $tmp = $namamhs;
+         }
+        else{
+            $tmp = "Something wrong. Please contact US";
+        }
+        return $tmp;
+    }
+
+    public function ifelse4($inputtgl = 'D'){
+        $tmp = '';
+        $d = date($inputtgl);
+        if($d == "Fri"){
+            $tmp = "Have a nice weekend!";
+        }elseif($d == "Sun"){
+            $tmp = "Have a nice weekend!";
+        }elseif($d == "Mon"){
+            $tmp = "Have a nice Monday!";
+        }elseif($d == "Tue"){
+            $tmp = "Have a nice Tuesday!";
+        }elseif($d == "Wed"){
+            $tmp = "Have a nice Wednesday!";
+        }elseif($d == "Thu"){
+            $tmp = "Have a nice Thursday!";
+        }elseif($d == "Sat"){
+            $tmp = "Have a nice Weekend!";
+        }
+        return $tmp;
+    }
+
+    public function loop1($var){
+        for ($i=0; $i <= 10; $i++) { 
+            $var+=$var;
+        }
+        return $var;
+    }
+
+    public function loop2($arr){
+        $result = '';
+        foreach ($arr as $key => $value) {
+            if($key % 2 == 1){
+                $value+=$value;
+            }
+            $result = $value;
+        }
+        return $result;
+    }
+
+    public function loop3($var){
+        $a=0;
+        while ($a <= 10) {
+            $var += $var;
+            $a++;
+        }
+        return $var;
     }
 }
