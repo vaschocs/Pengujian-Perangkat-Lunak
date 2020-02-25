97c0412fd3ca153f43fdb313982dc3efaf2009e3
Argo Wibowo <argo.wibowo1989@gmail.com>
2020-02-25 17:14:05 +0700

    Membuat Testing 4

---
+++ application/controllers/Testing4.php
@@ -0,0 +1,150 @@
+<?php
+
+class Testing4 extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("user_model");
+        //$this->load->library('form_validation');
+        $this->load->library('unit_test');
+        // $this->output->enable_profiler(TRUE);
+    }
+
+    public function index()
+    {
+        //template
+        $str = '
+            <table border="0" cellpadding="4" cellspacing="1">
+            {rows}
+                    <tr>
+                            <td>{item}</td>
+                            <td>{result}</td>
+                    </tr>
+            {/rows}
+            </table>';
+        //$this->unit->set_template($str);
+        $true = true;
+        $expected = true;
+        $test_name = 'uji coba assert';
+
+        //test url
+        //$output = $this->request('GET',['Login','test']);
+        $expect = '{"foo":"bar"}';
+
+        //$this->unit->run($output,$expect,$test_name);
+        $this->unit->run($true,$expected,$test_name);
+
+        $test_name = 'tes if else';
+        $this->unit->run($this->ifelse('tes','halo'),'tes',$test_name);
+
+        $test_name = 'tes loop 2';
+        $arr = array(0,1,2,3,4);
+        $this->unit->run($this->loop2($arr),4,$test_name);
+
+        echo $this->unit->report();
+    }
+
+    public function logout()
+    {
+        $this->session->sess_destroy();
+        redirect(site_url('admin/login'));
+    }
+
+    public function test()
+    {
+        $this->output
+        ->set_content_type('application/json')
+        ->set_output(json_encode(array('foo' => 'bar')));
+    }
+
+    //minggu 3
+    public function ifelse($namachief = null,$namarm = null){
+        $tmp = '';
+        if($namachief != NULL){
+            $tmp = $namachief;
+        }
+        else if ($namarm != NULL){
+           $tmp = $namarm;
+        }
+        else{
+            $tmp = "Something wrong. Please contact US";
+        }
+        return $tmp;
+    }
+
+    public function ifelse2($teman){
+        $tmp = '';
+        if($teman == "andi"){
+            $tmp = "dia adalah teman saya";
+        }else{
+            $tmp = "dia bukan teman saya";
+        }
+        return $tmp;
+    }
+
+    public function ifelse3($namachief = null, $namarm = null, $namamhs){
+        $tmp = '';
+        if($namachief != NULL){
+            $tmp = $namachief;
+        }
+        else if ($namarm != NULL){
+            $tmp = $namarm;
+        }
+        else if ($namamhs != NULL){
+            $tmp = $namamhs;
+         }
+        else{
+            $tmp = "Something wrong. Please contact US";
+        }
+        return $tmp;
+    }
+
+    public function ifelse4($inputtgl = 'D'){
+        $tmp = '';
+        $d = date($inputtgl);
+        if($d == "Fri"){
+            $tmp = "Have a nice weekend!";
+        }elseif($d == "Sun"){
+            $tmp = "Have a nice weekend!";
+        }elseif($d == "Mon"){
+            $tmp = "Have a nice Monday!";
+        }elseif($d == "Tue"){
+            $tmp = "Have a nice Tuesday!";
+        }elseif($d == "Wed"){
+            $tmp = "Have a nice Wednesday!";
+        }elseif($d == "Thu"){
+            $tmp = "Have a nice Thursday!";
+        }elseif($d == "Sat"){
+            $tmp = "Have a nice Weekend!";
+        }
+        return $tmp;
+    }
+
+    public function loop1($var){
+        for ($i=0; $i <= 10; $i++) { 
+            $var+=$var;
+        }
+        return $var;
+    }
+
+    public function loop2($arr){
+        $result = '';
+        foreach ($arr as $key => $value) {
+            if($key % 2 == 1){
+                $value+=$value;
+            }
+            $result = $value;
+        }
+        return $result;
+    }
+
+    public function loop3($var){
+        $a=0;
+        while ($a <= 10) {
+            $var += $var;
+            $a++;
+        }
+        return $var;
+    }
+}
